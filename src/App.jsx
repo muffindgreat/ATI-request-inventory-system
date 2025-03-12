0ee230835cad15833e5153a9169535b811bf2e09
johnbertchristiene <johnbertchristienetagle@gmail.com>
2025-03-12 09:53:42 +0800

    Moving MyRequests.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Login from "./components/LoginPage/Login";
 import Register from "./components/RegisterPage/Register";
 import Navbar from "./components/NavBar/Navbar";
-import MyRequests from "./components/MyRequestsPage/MyRequests";
+import MyRequests from "./pages/MyRequests/MyRequests";
 import MostViewed from "./components/LandingPage/MostViewed";
 import ItemInfo from "./components/ItemInfo/ItemDetails";
 import Library from "./components/LandingPage/Library";
@@ -12,9 +12,9 @@ function App() {
     <div>
       {/* <Login /> */}
       {/* <Register /> */}
-      {/* <MyRequests /> */}
+      <MyRequests />
       {/* <MostViewed /> */}
-      <ItemInfo />
+      {/* <ItemInfo /> */}
       {/* <Library /> */}
     </div>
   );
