b402b116f39c56b43d9cd415694d9b2cf1eac5bc
John Jayson Magboo <johnjaysont.magboo@gmail.com>
2025-03-12 13:26:33 +0800

    ALL

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import Register from "./components/RegisterPage/Register";
 import Navbar from "./components/NavBar/Navbar";
 import MyRequests from "./pages/MyRequests/MyRequests";
 import MostViewed from "./components/LandingPage/MostViewed";
-import ItemInfo from "./components/ItemInfo/ItemDetails";
 import Library from "./components/LandingPage/Library";
-<<<<<<< HEAD
-import UserProfile from "./components/UserProfile";
-=======
 import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
->>>>>>> origin/master
+import ItemInfo from "./components/ItemInfo/ItemInfo";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -18,13 +15,11 @@ function App() {
       {/* <Login /> */}
       {/* <Register /> */}
       {/* <MyRequests /> */}
-      {/* <MostViewed /> */}
+      <MostViewed />
       {/* <Library /> */}
-<<<<<<< HEAD
-      {/* <Navbar /> */}
-      <UserProfile />
-=======
       {/* <SearchFilterBar /> */}
+      {/* <ItemInfo /> */}
+      {/* <UserProfile /> */}
     </div>
   );
 }
