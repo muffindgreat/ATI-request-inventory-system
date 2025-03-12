311c471c567a903cb8b418d19364204fc956c732
Dyud07 <main.jude.bautista@cvsu.edu.ph>
2025-03-12 17:14:43 +0800

    Material Req Form

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import Register from "./components/RegisterPage/Register";
 import Navbar from "./components/NavBar/Navbar";
 import MyRequests from "./pages/MyRequests/MyRequests";
 import MostViewed from "./components/LandingPage/MostViewed";
-import ItemInfo from "./components/ItemInfo/ItemDetails";
-import Library from "./components/LandingPage/Library";
-<<<<<<< HEAD
-import UserProfile from "./components/UserProfile";
-=======
-import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
->>>>>>> origin/master
+//import ItemInfo from "./components/ItemInfo/ItemDetails";
+//import Library from "./components/LandingPage/Library";
+//import UserProfile from "./components/UserProfile";
+//import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
+import MatsReq from "./pages/MaterialRequisition/MatsReq";
+
 
 function App() {
   return (
@@ -21,7 +20,8 @@ function App() {
       {/* <MostViewed /> */}
       {/* <Library /> */}
       {/* <SearchFilterBar /> */}
->>>>>>> origin/master
+      <Navbar/>
+      <MatsReq/>
     </div>
   );
 }
