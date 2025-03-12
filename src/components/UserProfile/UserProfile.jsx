07ee0864e4bf2db7ae93ce972644e23e67039dcd
John Jayson Magboo <johnjaysont.magboo@gmail.com>
2025-03-12 14:50:02 +0800

    nav userprof

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Card, CardContent, Box } from "@mui/material";
+import { Card, CardContent, Box, Typography } from "@mui/material";
 import ProfileHeader from "./ProfileHeader";
 import ProfilePicture from "./ProfilePicture";
 import ProfileDetails from "./ProfileDetails";
 import EditProfileButton from "./EditProfileButton";
+import Navbar from "../NavBar/Navbar";
 
 const UserProfile = () => {
   const [isEditing, setIsEditing] = useState(false);
@@ -22,37 +23,54 @@ const UserProfile = () => {
   };
 
   return (
-    <Card
-      sx={{
-        maxWidth: 800,
-        margin: "auto",
-        marginTop: 4,
-        borderRadius: 2,
-        boxShadow: 3,
-      }}
-    >
-      <ProfileHeader />
-      <CardContent
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          gap: 4,
-          padding: 4,
-          alignItems: "center",
-        }}
-      >
-        <ProfilePicture />
-        <ProfileDetails
-          userData={userData}
-          handleInputChange={handleInputChange}
-          isEditing={isEditing}
-        />
-      </CardContent>
-      <EditProfileButton
-        isEditing={isEditing}
-        toggleEditing={() => setIsEditing(!isEditing)}
-      />
-    </Card>
+    <>
+      <Navbar />
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 6 }}>
+        {" "}
+        {/* Increased spacing */}
+        <Card
+          sx={{
+            maxWidth: 800,
+            width: "100%",
+            borderRadius: 2,
+            boxShadow: 3,
+            p: 3, // Added padding for better spacing
+          }}
+        >
+          {/* Properly spaced heading */}
+          <Typography
+            variant="h6"
+            sx={{ textAlign: "center", mb: 2 }}
+          ></Typography>
+          <Typography
+            variant="h6"
+            sx={{ textAlign: "center", mb: 2 }}
+          ></Typography>
+
+          <ProfileHeader />
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              gap: 4,
+              padding: 4,
+              alignItems: "center",
+            }}
+          >
+            <ProfilePicture />
+            <ProfileDetails
+              userData={userData}
+              handleInputChange={handleInputChange}
+              isEditing={isEditing}
+            />
+          </CardContent>
+          <EditProfileButton
+            isEditing={isEditing}
+            toggleEditing={() => setIsEditing(!isEditing)}
+          />
+        </Card>
+      </Box>
+    </>
   );
 };
 
