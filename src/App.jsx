a5a79b70505a6614fccc17afed1ab37c27c7ac40
johnbertchristiene <johnbertchristienetagle@gmail.com>
2025-03-13 10:23:50 +0800

    Log In page moved inside the pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
+import LogIn from "./pages/LogIn/LogIn";
 import Login from "./components/LoginPage/Login";
-import Register from "./components/RegisterPage/Register";
-import Navbar from "./components/NavBar/Navbar";
-import MyRequests from "./pages/MyRequests/MyRequests";
-import MostViewed from "./components/LandingPage/MostViewed";
+// import Register from "./components/RegisterPage/Register";
+// import Navbar from "./components/NavBar/Navbar";
+// import MyRequests from "./pages/MyRequests/MyRequests";
+// import MostViewed from "./components/LandingPage/MostViewed";
+// import Library from "./components/LandingPage/Library";
+// import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
+// // import ItemInfo from "./components/ItemInfo/ItemInfo";
+// import UserProfile from "./components/UserProfile/UserProfile";
+// import MatsReq from "./pages/MaterialRequisition/MatsReq";
+// import ItemInfo from "./components/ItemInfo/ItemDetails";
 import Library from "./components/LandingPage/Library";
-import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
-import ItemInfo from "./components/ItemInfo/ItemInfo";
 import UserProfile from "./components/UserProfile/UserProfile";
-import MatsReq from "./pages/MaterialRequisition/MatsReq";
-import ItemInfo from "./components/ItemInfo/ItemDetails";
-import Library from "./components/LandingPage/Library";
-import UserProfile from "./components/UserProfile";
 import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
 import MatsReq from "./pages/MaterialRequisition/MatsReq";
 
 function App() {
   return (
     <div>
-      {/* <Login /> */}
+      <LogIn />
       {/* <Register /> */}
       {/* <MyRequests /> */}
       {/* <MostViewed /> */}
