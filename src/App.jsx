5a056d54916b3afbe90d7cd176c03412f9261f49
johnbertchristiene <johnbertchristienetagle@gmail.com>
2025-03-14 09:52:16 +0800

    new app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,38 @@ import LogIn from "./pages/LogIn/LogIn";
 import Register from "./pages/Register/Register";
 import Navbar from "./components/NavBar/Navbar";
 import MyRequests from "./pages/MyRequests/MyRequests";
-// import MostViewed from "./components/LandingPage/MostViewed";
+import MostViewed from "./components/LandingPage/MostViewed";
 import Library from "./components/LandingPage/Library";
 import SearchFilterBar from "./components/ItemSort/SearchFilterBar";
 import ItemInfo from "./components/ItemInfo/ItemInfo";
 import UserProfile from "./pages/UserProfile/UserProfile";
 import MatsReq from "./pages/MaterialRequisition/MatsReq";
+import ItemDetails from "./components/ItemInfo/ItemDetails";
 
 function App() {
   return (
-    <div>
-      <LogIn />
-      {/* <Register /> */}
-      {/* <MyRequests /> */}
-      {/* <MostViewed /> */}
-      {/* <Library /> */}
-      {/* <SearchFilterBar /> */}
-      {/* <ItemInfo /> */}
-      {/* <UserProfile /> */}
-      {/* <MatsReq /> */}
-      {/* // <Navbar/>
-      // <MatsReq/> */}
-    </div>
+    <Router>
+      <Routes>
+        {/* <LogIn /> */}
+        {/* <Register /> */}
+        {/* <Navbar /> */}
+        {/* <MyRequests /> */}
+        {/* <MostViewed /> */}
+        {/* <Library /> */}
+        {/* <SearchFilterBar /> */}
+        {/* <ItemInfo /> */}
+        {/* <UserProfile /> */}
+        {/* <MatsReq /> */}
+
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/my-requests" element={<MyRequests />} />
+        <Route path="/most-viewed" element={<MostViewed />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/material-request" element={<MatsReq />} />
+      </Routes>
+    </Router>
   );
 }
 
