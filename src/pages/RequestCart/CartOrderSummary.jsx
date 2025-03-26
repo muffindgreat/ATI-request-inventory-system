ee4669dad05ef36893a94a446ef3812c2b5c6945
giieezy <teodoroiangabrielle@gmail.com>
2025-03-26 09:28:48 +0800

    fix

--- src/pages/RequestCart/CartOrderSummary.jsx
+++ src/pages/RequestCart/CartOrderSummary.jsx
@@ -163,7 +163,7 @@ export default function CartOrderSummary({
             textTransform: "none",
             p: 1,
           }}
-          onClick={() => navigate("/material-request")}
+          onClick={() => navigate("/material-request-form")}
         >
           Check Out
         </Button>
