9a58b54fbe967c94f568afacb3a9df8d4473d048
John Jayson Magboo <johnjaysont.magboo@gmail.com>
2025-04-08 14:31:37 +0800

    new na to

--- src/components/LandingPage/Name.jsx
+++ src/components/LandingPage/Name.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography, Box, styled } from "@mui/material";
 
+// Background wrapper
 const StyledBox = styled(Box)(({ theme }) => ({
   backgroundImage: `url('/books.jpg')`,
   backgroundSize: "cover",
@@ -18,67 +19,62 @@ const StyledBox = styled(Box)(({ theme }) => ({
   },
   padding: theme.spacing(10, 2),
   display: "flex",
-  flexDirection: "row",
+  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  gap: 0, // no gap between koob and title
-  [theme.breakpoints.down("sm")]: {
-    flexDirection: "column",
-    textAlign: "center",
-  },
+  rowGap: theme.spacing(2),
+  textAlign: "center",
 }));
 
+// Logos
 const KoobLogo = styled("img")(({ theme }) => ({
-  height: 200,
+  height: 100,
   position: "relative",
   zIndex: 2,
   margin: 0,
-  gap: 0,
+  padding: 0,
   [theme.breakpoints.down("sm")]: {
     height: 100,
   },
 }));
 
 const AtisLogo = styled("img")(({ theme }) => ({
-  height: 60,
+  height: 50,
   position: "relative",
   zIndex: 2,
-  margin: 0,
-  padding: 0,
-  display: "block",
+  marginRight: theme.spacing(1),
   [theme.breakpoints.down("sm")]: {
     height: 40,
   },
 }));
 
+// Title container
 const TitleTextContainer = styled(Box)(({ theme }) => ({
-  position: "relative",
-  zIndex: 2,
-  color: "white",
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  margin: 0,
-  padding: 0,
+  alignItems: "flex-start",
+  color: "white",
+  position: "relative",
+  zIndex: 2,
+  [theme.breakpoints.down("sm")]: {
+    alignItems: "center",
+  },
 }));
 
-const TitleRow = styled(Box)({
+const TitleRow = styled(Box)(({ theme }) => ({
   display: "flex",
-  alignItems: "center", // try "baseline" if text is slightly off
-  gap: 0,
-  margin: 0,
-  padding: 0,
-});
+  alignItems: "center",
+  marginBottom: theme.spacing(1),
+}));
 
 const StyledTitle = styled(Typography)(({ theme }) => ({
   fontFamily: "Montserrat, sans-serif",
-  fontWeight: 500,
+  fontWeight: 400,
   color: "white",
-  lineHeight: 1,
-  margin: 0,
-  padding: 0,
+  lineHeight: 1.2,
   [theme.breakpoints.down("sm")]: {
-    fontSize: "1.3rem",
+    fontSize: "1.5rem",
   },
   [theme.breakpoints.up("md")]: {
     fontSize: "2.5rem",
@@ -89,9 +85,7 @@ const StyledTitle1 = styled(Typography)(({ theme }) => ({
   fontFamily: "Montserrat, sans-serif",
   fontWeight: 700,
   color: "white",
-  lineHeight: 1,
-  margin: 0,
-  padding: 0,
+  lineHeight: 1.2,
   [theme.breakpoints.down("sm")]: {
     fontSize: "2.5rem",
   },
@@ -100,10 +94,19 @@ const StyledTitle1 = styled(Typography)(({ theme }) => ({
   },
 }));
 
+// Styled Subtitle with typewriter effect
 const StyledSubtitle = styled(Typography)(({ theme }) => ({
   fontFamily: "Montserrat, sans-serif",
   color: "white",
-  marginTop: theme.spacing(1),
+  marginTop: theme.spacing(0),
+  position: "relative",
+  zIndex: 2,
+  display: "inline-block",
+  overflow: "hidden", // Hide overflowed text
+  whiteSpace: "nowrap", // Prevent wrapping
+  width: "0", // Initially no width
+  borderRight: "3px solid white", // Simulate cursor
+  animation: "typing 3s steps(35) 1s forwards, blink 0.75s step-end infinite", // Adjusted timing and steps
   [theme.breakpoints.down("sm")]: {
     fontSize: "1rem",
   },
@@ -112,21 +115,60 @@ const StyledSubtitle = styled(Typography)(({ theme }) => ({
   },
 }));
 
+// Global CSS for keyframes (add this in your CSS file)
+const globalStyles = `
+  @keyframes typing {
+    0% {
+      width: 0;
+    }
+    100% {
+      width: 39%;
+    }
+  }
+
+  @keyframes blink {
+    50% {
+      border-color: transparent;
+    }
+  }
+`;
+
+// Add the global keyframes to the page
+const GlobalStyles = () => <style>{globalStyles}</style>;
+
 const Title = () => {
   return (
-    <StyledBox>
-      <KoobLogo src="/koob.png" alt="Koob Logo" />
-      <TitleTextContainer>
-        <TitleRow>
-          <AtisLogo src="/atis.png" alt="ATI Logo" />
-          <StyledTitle variant="h2">ATI-CALABARZON</StyledTitle>
-        </TitleRow>
-        <StyledTitle1 variant="h2">E-Library</StyledTitle1>
+    <>
+      <GlobalStyles />
+      <StyledBox>
+        {/* Top logo row */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 0,
+            position: "relative",
+            zIndex: 2,
+          }}
+        >
+          <KoobLogo src="/koob.png" alt="Koob Logo" />
+          <TitleTextContainer>
+            <TitleRow>
+              <AtisLogo src="/atis.png" alt="ATI Logo" />
+              <StyledTitle variant="h2">ATI-CALABARZON</StyledTitle>
+            </TitleRow>
+            <StyledTitle1 variant="h2">E-Library</StyledTitle1>
+          </TitleTextContainer>
+        </Box>
+
+        {/* Subtitle with typewriter animation */}
         <StyledSubtitle variant="subtitle1">
           Your online portal to agricultural knowledge and learning materials.
         </StyledSubtitle>
-      </TitleTextContainer>
-    </StyledBox>
+      </StyledBox>
+    </>
   );
 };
 
