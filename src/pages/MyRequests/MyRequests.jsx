2f03285aa0beec569ded45382625acc222089e81
johnbertchristiene <johnbertchristienetagle@gmail.com>
2025-04-08 17:42:29 +0800

    remarks added on my requests requests list

--- src/pages/MyRequests/MyRequests.jsx
+++ src/pages/MyRequests/MyRequests.jsx
@@ -84,6 +84,7 @@ export default function MyRequests() {
                 purpose: data.purpose || "",
                 program: data.program || "",
                 section: data.section || "",
+                remarks: data.remarks || "",
                 materials, // Updated with Inventory Data
               };
             })
