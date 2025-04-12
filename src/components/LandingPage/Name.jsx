8d99ec645e38b12ca72ddf75c8845aaabfdf7ebf
John Jayson Magboo <johnjaysont.magboo@gmail.com>
2025-04-12 19:13:25 +0800

    nabalik na

--- src/components/LandingPage/Name.jsx
+++ src/components/LandingPage/Name.jsx
@@ -14,11 +14,10 @@ const StyledBox = styled(Box)(({ theme }) => ({
     left: 0,
     width: "100%",
     height: "100%",
-    background: "linear-gradient(to bottom, rgba(0,0,0,0.6), rgba(0,0,0,0.3))",
-
+    background: "linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.4))",
     zIndex: 1,
   },
-  padding: theme.spacing(5, 2),
+  padding: theme.spacing(2, 2),
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
@@ -27,117 +26,60 @@ const StyledBox = styled(Box)(({ theme }) => ({
   textAlign: "center",
 }));
 
-// Logos
-const KoobLogo = styled("img")(({ theme }) => ({
+const AtisLogo = styled("img")(({ theme }) => ({
   height: 100,
   position: "relative",
   zIndex: 2,
-  margin: 0, // removed extra spacing
-  padding: 0, // just in case
+  marginBottom: theme.spacing(1.5),
   [theme.breakpoints.down("sm")]: {
-    height: 100,
+    height: 50,
   },
 }));
 
-const AtisLogo = styled("img")(({ theme }) => ({
-  height: 50,
-  position: "relative",
-  zIndex: 2,
-  marginRight: theme.spacing(1),
-  [theme.breakpoints.down("sm")]: {
-    height: 40,
-  },
-}));
-
-// Title container
-const TitleTextContainer = styled(Box)(({ theme }) => ({
+const TitleTextContainer = styled(Box)({
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
-  alignItems: "flex",
+  alignItems: "center",
   color: "white",
   position: "relative",
   zIndex: 2,
-  [theme.breakpoints.down("sm")]: {
-    alignItems: "center",
-  },
-}));
+});
 
-const TitleRow = styled(Box)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  marginBottom: theme.spacing(1),
-}));
-
-const StyledTitle = styled(Typography)(({ theme }) => ({
-  fontFamily: "Montserrat, sans-serif",
+const StyledTitle = styled(Typography)({
+  fontFamily: "'Lato', sans-serif",
   fontWeight: 400,
   color: "white",
   lineHeight: 1.2,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "1.5rem",
-  },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "2.5rem",
-  },
-}));
+  fontSize: "clamp(1.2rem, 4vw, 2rem)",
+});
 
-const StyledTitle1 = styled(Typography)(({ theme }) => ({
-  fontFamily: "Montserrat, sans-serif",
+const StyledTitle1 = styled(Typography)({
+  fontFamily: "'Lato', sans-serif",
   fontWeight: 600,
   color: "white",
   lineHeight: 1.2,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "2.5rem",
-  },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "3rem",
-  },
-}));
+  fontSize: "clamp(1.5rem, 5vw, 2.5rem)",
+});
 
 const StyledSubtitle = styled(Typography)(({ theme }) => ({
-  fontFamily: "Montserrat, sans-serif",
+  fontFamily: "'Lato', sans-serif",
   color: "white",
   marginTop: theme.spacing(0),
   position: "relative",
   zIndex: 2,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "1rem",
-  },
-  [theme.breakpoints.up("md")]: {
-    fontSize: "1.2rem",
-  },
+  fontSize: "clamp(0.9rem, 2.5vw, 1.1rem)",
 }));
 
-// Main Component
 const Title = () => {
   return (
     <StyledBox>
-      {/* Top logo row */}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          alignItems: "center",
-          justifyContent: "center",
-          gap: 0, // no space between KoobLogo and title
-          position: "relative",
-          zIndex: 2,
-        }}
-      >
-        {/* <KoobLogo src="/koob.png" alt="Koob Logo" /> */}
-        <TitleTextContainer>
-          <TitleRow>
-            <AtisLogo src="/atis.png" alt="ATI Logo" />
-            <StyledTitle variant="h2">
-              Agricultural Training Institute CALABARZON
-            </StyledTitle>
-          </TitleRow>
-          <StyledTitle1 variant="h2">E-Library</StyledTitle1>
-        </TitleTextContainer>
-      </Box>
-
-      {/* Subtitle below */}
+      <AtisLogo src="/atis.png" alt="ATI Logo" />
+      <TitleTextContainer>
+        <StyledTitle variant="h2">Agricultural Training Institute</StyledTitle>
+        <StyledTitle variant="h2">CALABARZON</StyledTitle>
+        <StyledTitle1 variant="h2">e-Library</StyledTitle1>
+      </TitleTextContainer>
       <StyledSubtitle variant="subtitle1">
         Your online portal to agricultural knowledge and learning materials.
       </StyledSubtitle>
